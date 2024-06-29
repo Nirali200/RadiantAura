36f077e548460901ac061e2f0734a9c033fd8be5
Nirali200 <darjinirali08@gmail.com>
2024-06-29 18:10:24 +0530

    databsase connection string added to env:

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -1,17 +1,9 @@
-// const mongoose = require('mongoose')
-// const connectionString =  "";
-// const connectDb = ()=> {
-//     mongoose.connect(connectionString).then(()=>console.log("Database connected")).catch((e) => console.log(e));
-// }
 
-// module.exports = connectDb ;
-
-// import mongoose from "mongoose";
 const mongoose = require('mongoose')
 
 const connectDb = async() => {
     try{
-        const connect = await mongoose.connect("mongodb+srv://darjinirali08:fvGy6pR49SD9c979@RadiantAura.5z5p4yp.mongodb.net/?retryWrites=true&w=majority&appName=User",{useNewUrlParser: true, useUnifiedTopology:true})
+        const connect = await mongoose.connect(process.env.CONNECTION_STRING,{useNewUrlParser: true, useUnifiedTopology:true})
         console.log(
             "Database connected: ",
             connect.connection.host,
